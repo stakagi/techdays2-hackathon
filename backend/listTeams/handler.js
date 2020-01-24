635c73236072f4e01259c543a38701b62abbac2c
Shunichi Takagi <shunichi_takagi@jbat.co.jp>
2020-01-24 07:07:29 +0000

    チーム一覧と詳細取得のAPIモックを作成

--- backend/listTeams/handler.js
+++ backend/listTeams/handler.js
@@ -1,18 +1,46 @@
 'use strict';
 
 module.exports.main = async event => {
+  console.log(event);
+  
   return {
     statusCode: 200,
     body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
+      [
+        {
+          team_id: '1',
+          team_name: 'チーム1',
+          title: 'タイトル（チーム1）',
+          keyword: 'キーワード',
+          movie_url: 'movie_url',
+          thumbnail_url: 'thumbnail_url',
+          members: 'メンバー一覧',
+          views: 0,
+          votes: 0
+        },
+        {
+          team_id: '2',
+          team_name: 'チーム2',
+          title: 'タイトル（チーム2）',
+          keyword: 'キーワード',
+          movie_url: 'movie_url',
+          thumbnail_url: 'thumbnail_url',
+          members: 'メンバー一覧',
+          views: 0,
+          votes: 0
+        },
+        {
+          team_id: '3',
+          team_name: 'チーム3',
+          title: 'タイトル（チーム3）',
+          keyword: 'キーワード',
+          movie_url: 'movie_url',
+          thumbnail_url: 'thumbnail_url',
+          members: 'メンバー一覧',
+          views: 0,
+          votes: 0
+        }
+      ]
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
