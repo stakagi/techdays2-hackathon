35a2775d53e6378bc6f64888349c1e6f5b73d5bf
s_hasegawa <s_hasegawa@jbat.co.jp>
2020-01-24 16:56:24 +0900

    feat: add iroiro.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ListPageComponent } from './list-page/list-page.component';
 import { ViewerPageComponent } from './viewer-page/viewer-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: 'list', component: ListPageComponent },
-  { path: 'viewer/:teamId', component: ViewerPageComponent }
+  { path: 'viewer/:teamId', component: ViewerPageComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundPageComponent }
 ];
 
 @NgModule({
